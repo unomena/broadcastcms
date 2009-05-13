6bcffe0b531c4acbc79a6e9ae3cef5f8bea825d0
Shaun Sephton <eddied@noidea.(none)>
2009-05-13 17:56:31 +0200

    reworked workflow to permissions. setup permitted_objects manager

--- contentbase/admin.py
+++ contentbase/admin.py
@@ -1,8 +1,5 @@
 from django.contrib import admin
 from broadcastcms.shortcuts import comma_seperated_admin_links
-from django.db import models
-
-from broadcastcms.workflow.admin import WorkflowedObjectAdmin
 
 def comma_seperated_admin_label_links(obj):
     return comma_seperated_admin_links(obj.labels.all())
@@ -10,6 +7,5 @@ def comma_seperated_admin_label_links(obj):
 comma_seperated_admin_label_links.short_description = 'Labels'
 comma_seperated_admin_label_links.allow_tags = True
 
-
-class ContentBaseAdmin(WorkflowedObjectAdmin):
+class ContentBaseAdmin(admin.ModelAdmin):
     list_display = ('title', 'description', comma_seperated_admin_label_links, 'is_public')
--- contentbase/models.py
+++ contentbase/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
-from broadcastcms.workflow.models import WorkflowedObject
+from broadcastcms.permissionbase.models import PermissionBase
 from broadcastcms.label.models import Label
 
-class ContentBase(WorkflowedObject):
+class ContentBase(PermissionBase):
     title = models.CharField(max_length='512')
     description = models.TextField()
     labels = models.ManyToManyField(Label, blank=True)
