f6cabc7b458865bd2eb560c6a37eeca21c2c90d6
Shaun Sephton <eddied@noidea.(none)>
2009-05-13 11:53:22 +0200

    refactor

--- workflow/models.py
+++ workflow/models.py
@@ -1,7 +1,14 @@
 from django.db import models
+from django.template import Context
+
+class WorkflowedObjectManager(models.Manager):
+
+    def get_query_set(self):
+        return super(WorkflowedObjectManager, self).get_query_set().filter(is_public=True)
 
 class WorkflowedObject(models.Model):
-    public = models.BooleanField(default=False)
+    is_public = models.BooleanField(default=False, verbose_name="Public")
+    objects = WorkflowedObjectManager() 
 
     class Meta:
         abstract = True
