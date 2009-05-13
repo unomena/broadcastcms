87c977f28884014dfe67baae3f6111e2bc22a4e2
Shaun Sephton <eddied@noidea.(none)>
2009-05-13 06:22:14 +0200

    contentbase/post admin refactor

---
+++ contentbase/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from broadcastcms.shortcuts import comma_seperated_admin_links
+
+def comma_seperated_admin_label_links(obj):
+    return comma_seperated_admin_links(obj.labels.all())
+
+comma_seperated_admin_label_links.short_description = 'Labels'
+comma_seperated_admin_label_links.allow_tags = True
+
+class ContentBaseAdmin(admin.ModelAdmin):
+    list_display = ('title', comma_seperated_admin_label_links)
--- post/admin.py
+++ post/admin.py
@@ -1,14 +1,8 @@
 from django.contrib import admin
+from broadcastcms.contentbase.admin import ContentBaseAdmin
 from broadcastcms.post.models import Post
-from broadcastcms.shortcuts import comma_seperated_admin_links
 
-def comma_seperated_admin_label_links(obj):
-    return comma_seperated_admin_links(obj.labels.all())
-
-comma_seperated_admin_label_links.short_description = 'Labels'
-comma_seperated_admin_label_links.allow_tags = True
-
-class PostAdmin(admin.ModelAdmin):
-    list_display = ('title', comma_seperated_admin_label_links)
+class PostAdmin(ContentBaseAdmin):
+    pass
 
 admin.site.register(Post, PostAdmin)
