87ae2f160c1a5f617d86e5c6a8858d780592f44c
Shaun Sephton <eddied@noidea.(none)>
2009-05-22 12:37:24 +0200

    show model work. renamed label visible field to is_visible

--- show/models.py
+++ show/models.py
@@ -1,5 +1,12 @@
 from django.db import models
+from broadcastcms.label.models import Label
 from broadcastcms.base.models import ContentBase
 
 class Show(ContentBase):
-    pass
+    image_scales = ((170, 96),)
+    extended_description = models.TextField(verbose_name='Extended Description')
+    genres = models.ManyToManyField(Label, blank=True)
+
+    class Meta():
+        verbose_name = 'Show'
+        verbose_name_plural = 'Shows'
