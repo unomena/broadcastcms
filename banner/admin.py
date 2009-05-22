6ea7a085920c983acbf9004e50e27ab81c0623cc
Shaun Sephton <eddied@noidea.(none)>
2009-05-22 09:29:20 +0200

    added banner admin. removed .gitignore - developer should set his own

--- banner/admin.py
+++ banner/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from models import ImageBanner
+from models import CodeBanner, ImageBanner
 
-class ImageBannerAdmin(admin.ModelAdmin):
+class BannerAdmin(admin.ModelAdmin):
     list_display = ('title', 'is_public')
     list_filter = ('is_public',)
     search_fields = ('title',)
-
-admin.site.register(ImageBanner, ImageBannerAdmin)
+    
+admin.site.register(CodeBanner, BannerAdmin)
+admin.site.register(ImageBanner, BannerAdmin)
