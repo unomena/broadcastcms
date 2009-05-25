1fc062b3329879e3a56af4df2c4183cc8f76d9c6
Shaun Sephton <eddied@noidea.(none)>
2009-05-25 13:11:30 +0200

    castmember model work. show model additions

--- show/admin.py
+++ show/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from models import Show
+from models import Show, CastMember
 
 class ShowAdmin(admin.ModelAdmin):
     pass
 
+class CastMemberAdmin(admin.ModelAdmin):
+    pass
+
 admin.site.register(Show, ShowAdmin)
+admin.site.register(CastMember, CastMemberAdmin)
--- show/models.py
+++ show/models.py
@@ -1,12 +1,28 @@
 from django.db import models
 from broadcastcms.label.models import Label
-from broadcastcms.base.models import ContentBase
+from broadcastcms.base.models import ModelBase, ContentBase
+
+class CastMember(ModelBase):
+    title = models.CharField(max_length="512")
+   
+    def __unicode__(self):
+        return str(self.title)
+
+    class Meta():
+        verbose_name = 'Cast Member'
+        verbose_name_plural = 'Cast Members'
 
 class Show(ContentBase):
-    image_scales = ((170, 96),)
+    image_scales = ((170, 96), (283, 159))
     extended_description = models.TextField(verbose_name='Extended Description')
     genres = models.ManyToManyField(Label, blank=True)
+    rating = models.CharField(max_length="128", blank=True, default="All Ages")
+    cast_members = models.ManyToManyField(CastMember, blank=True)
+    homepage_url = models.URLField(max_length='512', blank=True, verbose_name="Homepage URL")
 
+    def __unicode__(self):
+        return str(self.title)
+    
     class Meta():
         verbose_name = 'Show'
         verbose_name_plural = 'Shows'
