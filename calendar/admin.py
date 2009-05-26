6f242e3a83d2ff4a3677b5b4ec3df685d8368f7b
Jurie-Jan Botha <juriejanbotha@gmail.com>
2009-05-26 09:40:45 +0200

    Moved timeslot to entry in calendar application

---
+++ calendar/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from models import *
+
+
+class EntryInline(admin.TabularInline):
+    model = Entry
+    fk_name = 'content'
+    extra = 1
+
+
+class EntryAdmin(admin.ModelAdmin):
+    list_display = ('start_date_time', 'end_date_time', 'content', 'is_public')
+    list_filter = ('content', 'is_public',)
+    search_fields = ('start_date_time', 'end_date_time', 'content')
+
+
+admin.site.register(Entry, EntryAdmin)
---
+++ calendar/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from broadcastcms.base.models import ModelBase, ContentBase
+
+
+class Entry(ModelBase):
+    start_date_time = models.DateTimeField(verbose_name="Starting Date & Time")
+    end_date_time = models.DateTimeField(verbose_name="Ending Date & Time")
+    content = models.ForeignKey(ContentBase, limit_choices_to={'is_public': True}, verbose_name='Content')
+
+    def __unicode__(self):
+        return str(self.start_date_time)
