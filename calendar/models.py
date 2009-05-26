8b1f0c92780f08b3d243d517c22db9a70526a391
Jurie-Jan Botha <juriejanbotha@gmail.com>
2009-05-26 11:56:26 +0200

    Set the related name for claendar entry

--- calendar/models.py
+++ calendar/models.py
@@ -5,7 +5,7 @@ from broadcastcms.base.models import ModelBase, ContentBase
 class Entry(ModelBase):
     start_date_time = models.DateTimeField(verbose_name="Starting Date & Time")
     end_date_time = models.DateTimeField(verbose_name="Ending Date & Time")
-    content = models.ForeignKey(ContentBase, limit_choices_to={'is_public': True}, verbose_name='Content')
+    content = models.ForeignKey(ContentBase, limit_choices_to={'is_public': True}, verbose_name='Content', related_name='entries')
 
     def __unicode__(self):
         return str(self.start_date_time)
