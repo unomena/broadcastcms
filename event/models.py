33a6a97c6b1142f3468ec8b357843991f491fe42
Jurie-Jan Botha <juriejanbotha@gmail.com>
2009-05-26 17:23:01 +0200

    Added location model for events

--- event/models.py
+++ event/models.py
@@ -1,8 +1,16 @@
 from django.db import models
-from broadcastcms.base.models import ContentBase
+from broadcastcms.base.models import ContentBase, ModelBase
+
+
+class Location(ModelBase):
+    name = models.CharField(max_length=255)
+
+    def __unicode__(self):
+        return self.name
 
 
 class Event(ContentBase):
     image_scales = ()
     venue = models.CharField(max_length=255)
     address = models.CharField(max_length=512)
+    locations = models.ManyToManyField(Location, related_name='events')
