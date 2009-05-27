14719332fc13afd1ff36ffa8c1ffbcae7dbb0e3f
Jurie-Jan Botha <juriejanbotha@gmail.com>
2009-05-27 20:51:17 +0200

    Added percentage for poll option

--- poll/models.py
+++ poll/models.py
@@ -3,6 +3,9 @@ from broadcastcms.base.models import ContentBase, ModelBase
 
 
 class Poll(ContentBase):
+    image_scales = (
+        (111, 63),
+    )
     class Meta:
         verbose_name = 'Poll'
         verbose_name_plural = 'Polls'
@@ -12,6 +15,7 @@ class Option(ModelBase):
     poll = models.ForeignKey(Poll, related_name='options')
     title = models.CharField(max_length=512)
     votes = models.PositiveIntegerField(default=0)
+    percentage = models.FloatField(default=0)
 
     class Meta:
         verbose_name = 'Option'
@@ -19,3 +23,14 @@ class Option(ModelBase):
 
     def __unicode__(self):
         return '%s - %s' % (self.poll, self.title)
+
+    def save(self, *args, **kwargs):
+        super(Option, self).save(*args, **kwargs)
+        # set the percentages for all optoins relative to this one
+        options = self.poll.options.all()
+        total = float(0)
+        for option in options: total += option.votes
+        if total != 0:
+            for option in options:
+                option.percentage = (option.votes / total) * 100
+                super(Option, option).save()
