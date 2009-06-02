6107a64ca9a86c1a3d263cb4b4db845c187ce524
Shaun Sephton <eddied@noidea.(none)>
2009-06-02 10:29:37 +0200

    initial show admin work

--- show/admin.py
+++ show/admin.py
@@ -1,10 +1,23 @@
 from django.contrib import admin
 from broadcastcms.calendar.admin import EntryInline
+from broadcastcms.base.admin import ContentBaseAdmin
 from models import Show, CastMember
 
 
-class ShowAdmin(admin.ModelAdmin):
+class ShowAdmin(ContentBaseAdmin):
     inlines = (EntryInline,)
+    fieldsets = (
+        (None, {'fields': ('title', 'description', 'extended_description', 'rating', 'homepage_url', 'is_public')}),
+        ('Labels', {'fields': ('labels', 'genres'),
+                    'classes': ('collapse',),
+        }),
+        ('Cast & Crew', {'fields': ('cast_members',),
+                    'classes': ('collapse',),
+        }),
+        ('Meta', {'fields': ('image', 'created',),
+                  'classes': ('collapse',),
+        }),
+    )
 
 
 admin.site.register(Show, ShowAdmin)
