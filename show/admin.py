3eafc3c550f129babc5810756060c499ebd68887
Shaun Sephton <eddied@noidea.(none)>
2009-06-09 19:11:12 +0200

    show admin additions

--- show/admin.py
+++ show/admin.py
@@ -1,10 +1,19 @@
 from django.contrib import admin
 from broadcastcms.calendar.admin import EntryInline
-from broadcastcms.base.admin import ContentBaseAdmin
+from broadcastcms.base.admin import ContentBaseAdmin, comma_seperated_admin_label_links
+from broadcastcms.shortcuts import comma_seperated_admin_links
 from models import Show, CastMember
 
+def comma_seperated_admin_genre_links(obj):
+    return comma_seperated_admin_links(obj.genres.all())
+
+comma_seperated_admin_genre_links.short_description = 'Genres'
+comma_seperated_admin_genre_links.allow_tags = True
 
 class ShowAdmin(ContentBaseAdmin):
+    list_display = ('title', 'description', comma_seperated_admin_label_links, comma_seperated_admin_genre_links, 'created', 'modified', 'is_public')
+    list_filter = ('labels', 'genres', 'is_public', 'created', 'modified')
+    
     inlines = (EntryInline,)
     fieldsets = (
         (None, {'fields': ('title', 'description', 'extended_description', 'rating', 'homepage_url', 'is_public')}),
