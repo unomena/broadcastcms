09f7056d5df3e5c1423f3510550fa9d0fabaf19a
Shaun Sephton <eddied@noidea.(none)>
2009-06-09 10:22:48 +0200

    refactored as_leaf_class to not issue a db query

--- base/models.py
+++ base/models.py
@@ -80,18 +80,17 @@ class ModelBase(PermissionBase):
     default_manager = ModelBaseManager()
 
     content_type = models.ForeignKey(ContentType,editable=False,null=True)
+    classname = models.CharField(max_length=32, editable=False, null=True)
 
     def save(self, *args, **kwargs):
         if(not self.content_type):
             self.content_type = ContentType.objects.get_for_model(self.__class__)
+        
+        self.classname = self.__class__.__name__
         super(ModelBase, self).save(*args, **kwargs)
 
     def as_leaf_class(self):
-        content_type = self.content_type
-        model = content_type.model_class()
-        if(model == ContentBase):
-            return self
-        return model.objects.get(id=self.id)
+        return self.__getattribute__(self.classname.lower())
 
 
 class ContentBase(ModelBase):
