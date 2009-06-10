5f48bd02d430769b446fe0c0f223e28c4829ecdc
Shaun Sephton <eddied@noidea.(none)>
2009-06-10 14:43:32 +0200

    added helpt text to labels

--- label/models.py
+++ label/models.py
@@ -2,9 +2,9 @@ from django.db import models
 from broadcastcms.fields import modelfields
 
 class Label(models.Model):
-    title = models.CharField(max_length='64')
-    restricted_to = modelfields.CommaSeperatedCharField(blank=True)
-    is_visible = models.BooleanField(default=True, verbose_name='Visible')
+    title = models.CharField(max_length='64', help_text='A short descriptive title.')
+    restricted_to = modelfields.CommaSeperatedCharField(blank=True, help_text="Field(s) to which this label will be restricted. This allows you to have per type/field specific labels. So for instance if you select 'Show - Labels' this label will only be available for shows under labels.")
+    is_visible = models.BooleanField(default=True, verbose_name='Visible', help_text="Check to make this label visible to the public. Labels that are not visible will never be displayed on the public facing portal.")
 
     class Meta():
         verbose_name = 'Label'
