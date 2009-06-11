22eab771517d12a9e20c636514e3bdb5ec56dfe8
Shaun Sephton <eddied@noidea.(none)>
2009-06-11 17:03:21 +0200

    cache decorator updated to respect get parameters

--- cache/decorators.py
+++ cache/decorators.py
@@ -1,19 +1,27 @@
 from django.core.cache import cache
     
 
-def get_inclusion_view_function_cache_key(node, context, respect_path, f):
+def get_inclusion_view_function_cache_key(node, context, respect_path, respect_get, f):
+    """
+    TODO: Refactor to generate keys more succinctly.
+    """
     key = str(node.__class__) + str(f.__name__)
+    request = context['request']
     if respect_path:
-        request = context['request']
-        path_info = request.META['PATH_INFO']
-        key += path_info
+        path_key = str(request.path)
+        key += path_key
+    if respect_get:
+        #TODO: Generate a proper get_key instead of a simple string
+        get_key = str(request.GET)
+        key += get_key
+    
     key = key.replace(' ', '')
     return key
 
-def cache_inclusion_view_function(seconds, respect_path=False):
+def cache_inclusion_view_function(seconds, respect_path=False, respect_get=False):
     def wrap(f):
         def wrap_f(self, context):
-            key = get_inclusion_view_function_cache_key(self, context, respect_path, f)
+            key = get_inclusion_view_function_cache_key(self, context, respect_path, respect_get, f)
             cached_result = cache.get(key)
             if cached_result:
                 return cached_result
