0ec076dd8b7fbde1395b643f981a62b1a4d41009
Jurie-Jan Botha <juriejanbotha@gmail.com>
2009-07-02 12:49:48 +0200

    Eggified

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='broadcastcms',
+    version='dev',
+    #version='0.0.3',
+    description='Broadcast CMS Django Applications',
+    author='Praekelt Consulting',
+    author_email='sysadmin@praekeltconsulting.com',
+    url='http://projects.praekelt.com/broadcastcms',
+    packages = find_packages(),
+)
