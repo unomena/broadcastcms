7c572e15aad36c7cc47858281d4991951227cd5f
Jurie-Jan Botha <juriejanbotha@gmail.com>
2009-07-21 07:15:54 +0200

    Added exception catch for tag

--- broadcastcms/scaledimage/templatetags/scaled_image_tags.py
+++ broadcastcms/scaledimage/templatetags/scaled_image_tags.py
@@ -1,6 +1,8 @@
 from django import template
 
+
 register = template.Library()
+
 
 class ScaledImageURLNode(template.Node):
     def __init__(self, obj, width=None, height=None):
@@ -12,17 +14,21 @@ class ScaledImageURLNode(template.Node):
         width, height = self.width, self.height
         obj = self.obj.resolve(context)
         image = obj.image
-        original_url = image.url
-        if width and height:
-            if '.' in original_url:
-                dot_index = original_url.rindex('.')
-                scaled_name = '%sx%s%s' % (width, height, original_url[dot_index:])
+        try:
+            original_url = image.url
+            if width and height:
+                if '.' in original_url:
+                    dot_index = original_url.rindex('.')
+                    scaled_name = '%sx%s%s' % (width, height, original_url[dot_index:])
+                else:
+                    scaled_name = '%sx%s' % (width, height)
+                scaled_url = '%s/%s' % ('/'.join(original_url.split('/')[:-1]), scaled_name)
+                return scaled_url
             else:
-                scaled_name = '%sx%s' % (width, height)
-            scaled_url = '%s/%s' % ('/'.join(original_url.split('/')[:-1]), scaled_name)
-            return scaled_url
-        else:
-            return original_url
+                return original_url
+        except:
+            return ''
+
 
 @register.tag       
 def scaled_image_url(parser, token):
