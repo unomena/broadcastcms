62dd8d63b86a8b2638d2f5915832cb543e8f5686
Shaun Sephton <eddied@noidea.(none)>
2009-07-23 10:36:52 +0200

    usernamefield now lowers value

--- broadcastcms/fields/formfields.py
+++ broadcastcms/fields/formfields.py
@@ -8,8 +8,10 @@ class UsernameField(forms.CharField):
 
     def clean(self, value):
         """
+        Lowers value.
         Checks that the value does not include non-alphanumeric characters, excluding "_"
         """
+        value = value.lower()
         if re.search(r'\W', value):
             raise ValidationError(self.error_messages['invalid'])
 
