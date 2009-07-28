ba8cd253f598a2e7bccc53694ebd77db9d42ace4
Jurie-Jan Botha <juriejanbotha@gmail.com>
2009-07-28 08:06:46 +0200

    Created basic radio application

---
+++ broadcastcms/radio/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+from broadcastcms.base.models import ContentBase, ModelBase
+from broadcastcms.richtext.fields import RichTextField
+
+
+class Artist(ContentBase):
+    content = RichTextField()
+    songs = models.ManyToManyField('Song', through='Credit')
+
+
+class Song(ModelBase):
+    title = models.CharField(max_length=255)
+    current_position = models.IntegerField()
+    previous_position = models.IntegerField()
+    artists = models.ManyToManyField('Artist', through='Credit')
+
+
+class Credit(models.Model):
+    artist = models.ForeignKey(Artist, related_name='credits')
+    song = models.ForeignKey(Song, related_name='credits')
+    role = models.CharField(max_length=255)
