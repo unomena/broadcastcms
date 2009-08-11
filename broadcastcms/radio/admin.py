0b9a33dfba51c14c418c949cce4cbb171a1c7bf1
Shaun Sephton <eddied@noidea.(none)>
2009-08-11 21:06:18 +0200

    added credit inline

--- broadcastcms/radio/admin.py
+++ broadcastcms/radio/admin.py
@@ -1,8 +1,20 @@
 from django.contrib import admin
 
+from broadcastcms.base.admin import ContentBaseAdmin, ModelBaseTabularInline
+
 from models import *
 
+class CreditInline(ModelBaseTabularInline):
+    model = Credit
+    extra = 1
 
-admin.site.register(Artist)
-admin.site.register(Song)
+class ArtistAdmin(admin.ModelAdmin):
+    inlines = (CreditInline,)
+
+class SongAdmin(admin.ModelAdmin):
+    inlines = (CreditInline,)
+
+
+admin.site.register(Artist, ArtistAdmin)
+admin.site.register(Song, SongAdmin)
 admin.site.register(Credit)
