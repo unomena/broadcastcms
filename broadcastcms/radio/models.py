f1f97d15731f47dcecd80626627bca5bd90a1873
Shaun Sephton <eddied@noidea.(none)>
2009-08-11 21:06:55 +0200

    Song now inherits from ContentBase. Added credit unicode method

--- broadcastcms/radio/models.py
+++ broadcastcms/radio/models.py
@@ -9,17 +9,15 @@ class Artist(ContentBase):
     songs = models.ManyToManyField('Song', through='Credit')
 
 
-class Song(ModelBase):
-    title = models.CharField(max_length=255)
+class Song(ContentBase):
     album = models.CharField(max_length=255, blank=True, null=True)
     artists = models.ManyToManyField('Artist', through='Credit')
     video_url = models.CharField('Video URL', max_length=255, blank=True, null=True)
-
-    def __unicode__(self):
-        return self.title
-
 
 class Credit(models.Model):
     artist = models.ForeignKey(Artist, related_name='credits')
     song = models.ForeignKey(Song, related_name='credits')
     role = models.CharField(max_length=255, blank=True, null=True)
+
+    def __unicode__(self):
+        return "%s credit for %s" % (self.artist.title, self.song.title)
