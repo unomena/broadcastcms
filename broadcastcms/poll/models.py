3a80b41380a0e99bbb1032f33e75ce689ece5ef4
Shaun Sephton <eddied@noidea.(none)>
2009-08-19 11:22:09 +0200

    added vote methods and corrected clas rename issue

--- broadcastcms/poll/models.py
+++ broadcastcms/poll/models.py
@@ -6,6 +6,32 @@ class Poll(ContentBase):
     class Meta:
         verbose_name = 'Poll'
         verbose_name_plural = 'Polls'
+    
+    def vote(self, option_id):
+        option = self.options.permitted().get(id=option_id)
+        
+        if not option:
+            # TODO: raise a vote error
+            return False
+        else:
+            option.votes += 1
+            option.save()
+            return True
+
+    def set_cookie(self, request, response):
+        voted_polls = request.COOKIES.get('voted_polls', None)
+        if not voted_polls: 
+            voted_polls = []
+        else: 
+            voted_polls = voted_polls.split(',')
+
+        voted_polls.append(str(self.id))
+        response.set_cookie('voted_polls', value=','.join(voted_polls))
+        return response
+
+    def voted(self, request):
+        cookies = request.COOKIES
+        return str(self.id) in cookies.get('voted_polls', '').split(',')
 
 
 class PollOption(ModelBase):
@@ -22,7 +48,7 @@ class PollOption(ModelBase):
         return '%s - %s' % (self.poll, self.title)
 
     def save(self, *args, **kwargs):
-        super(Option, self).save(*args, **kwargs)
+        super(PollOption, self).save(*args, **kwargs)
         # set the percentages for all optoins relative to this one
         options = self.poll.options.all()
         total = float(0)
@@ -30,4 +56,4 @@ class PollOption(ModelBase):
         if total != 0:
             for option in options:
                 option.percentage = (option.votes / total) * 100
-                super(Option, option).save()
+                super(PollOption, option).save()
