299b05347fffbfaa0da2833df92ec450ba2bf6ad
Shaun Sephton <eddied@noidea.(none)>
2009-08-26 15:15:55 +0200

    version bump

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup, find_packages
 
 setup(
     name='broadcastcms',
-    version='dev',
-    #version='1.0.5',
+    #version='dev',
+    version='1.0.6',
     description='Broadcast CMS Django Applications',
     author='Praekelt Consulting',
     author_email='sysadmin@praekeltconsulting.com',
