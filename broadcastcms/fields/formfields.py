27bf6d4b81b5afda4c6d85e83d26fcfa8e9acf58
Shaun Sephton <eddied@noidea.(none)>
2009-09-07 09:30:35 +0200

    added length, and exiting check on username field

--- broadcastcms/fields/formfields.py
+++ broadcastcms/fields/formfields.py
@@ -1,6 +1,7 @@
 import re
 
 from django import forms
+from django.contrib.auth.models import User
 from django.forms.util import ValidationError
 
 
@@ -9,10 +10,18 @@ class UsernameField(forms.CharField):
     def clean(self, value):
         """
         Lowers value.
-        Checks that the value does not include non-alphanumeric characters, excluding "_"
+        Checks that value is at least 4 characters long.
+        Checks that the value does not include non-alphanumeric characters, excluding "_".
+        Checks that a username of the given value does not already exist.
         """
         value = value.lower()
+        if len(value) < 4:
+            raise ValidationError(self.error_messages['invalid_length'])
+        
         if re.search(r'\W', value):
-            raise ValidationError(self.error_messages['invalid'])
+            raise ValidationError(self.error_messages['invalid_characters'])
 
+        if User.objects.filter(username__exact=value):
+            raise ValidationError(self.error_messages['existing'])
+        
         return super(UsernameField, self).clean(value)
