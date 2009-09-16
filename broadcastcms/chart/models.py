c6eddb6bc890b370242615e386aad89b48b357ae
Shaun Sephton <eddied@noidea.(none)>
2009-09-16 08:33:33 +0200

    added created field, reworked Meta, and override save to set created

--- broadcastcms/chart/models.py
+++ broadcastcms/chart/models.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from django.db import models
 
 from broadcastcms.base.models import ContentBase, ModelBase
@@ -12,15 +14,31 @@ class Chart(ContentBase):
 
 
 class ChartEntry(ModelBase):
-    chart = models.ForeignKey(Chart, related_name='chartentries')
-    song = models.ForeignKey(Song, related_name='chartentries')
+    chart = models.ForeignKey(
+        Chart, 
+        related_name='chartentries'
+    )
+    song = models.ForeignKey(
+        Song, 
+        related_name='chartentries'
+    )
     current_position = models.IntegerField()
     previous_position = models.IntegerField()
+    created = models.DateTimeField(
+        'Created Date & Time', 
+        blank=True,
+        help_text='Date and time on which this item was created. This is automatically set on creation, but can be changed subsequently.'
+    )
 
     class Meta:
-        verbose_name = 'Song'
-        verbose_name_plural = 'Songs'
+        verbose_name = 'Chart Entry'
+        verbose_name_plural = 'Chart Entries'
         ordering = ['current_position',]
 
     def __unicode__(self):
         return '%s entry %s' % (self.chart.title, self.current_position)
+    
+    def save(self, *args, **kwargs):
+        if not self.id and not self.created:
+            self.created = datetime.now()
+        super(ChartEntry, self).save(*args, **kwargs)
