7b3dbb8b1b27a36be58c2df6063b6fa444451e87
Shaun Sephton <eddied@noidea.(none)>
2009-09-16 08:32:27 +0200

    register refactor

--- broadcastcms/public/site.py
+++ broadcastcms/public/site.py
@@ -1,15 +1,14 @@
 import types
+import inspect
 
 class PublicSite(object):
     def register(self, model, view_class):
-        model_attr_names = dir(model)
-        view_attr_names = dir(view_class)
+        view_class_methods = inspect.getmembers(view_class, inspect.ismethod)
 
-        for attr_name in view_attr_names:
-            if attr_name not in model_attr_names:
-                attr = getattr(view_class, attr_name)
-                if isinstance(attr, types.UnboundMethodType):
-                    # TODO: verify the method parameters and perhaps the name
-                    setattr(model, attr_name, attr.im_func)
+        for method in view_class_methods:
+            name = method[0]
+            attr = method[1]
+            if isinstance(attr, types.UnboundMethodType):
+                setattr(model, name, attr.im_func)
 
 site = PublicSite()
