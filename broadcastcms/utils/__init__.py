8a8591b452ba1badc589650cbb9c6c899648a68b
Shaun Sephton <eddied@noidea.(none)>
2009-09-23 12:11:51 +0200

    removed get or create profile

--- broadcastcms/utils/__init__.py
+++ broadcastcms/utils/__init__.py
@@ -4,16 +4,6 @@ from django.core.mail import EmailMessage
 from django.contrib.sites.models import Site
 from django.core.exceptions import ObjectDoesNotExist
 
-def get_or_create_profile(user):
-    try:
-        profile = user.get_profile()
-    except ObjectDoesNotExist:
-        app, model = settings.AUTH_PROFILE_MODULE.split('.')
-        ProfileModel = get_model(app, model)
-        profile = ProfileModel(user=user)
-        profile.save()
-    return profile
-
 def mail_user(subject, message, user, content_subtype=None, fail_silently=False):
     current_site = Site.objects.get_current()
     site_name = current_site.name
