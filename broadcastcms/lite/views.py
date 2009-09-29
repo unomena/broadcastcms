9487ad356313d441c21892ca3158c2aa237caf14
Shaun Sephton <eddied@noidea.(none)>
2009-09-29 17:53:56 +0200

    added comment add view

--- broadcastcms/lite/views.py
+++ broadcastcms/lite/views.py
@@ -2,7 +2,10 @@ from datetime import date, datetime, timedelta
 import calendar
 
 from django.contrib import auth
+from django.contrib import comments
 from django.contrib.auth.models import User
+from django.contrib.comments import signals
+from django.contrib.comments.views.comments import CommentPostBadRequest
 from django.contrib.sites.models import Site
 from django.core.urlresolvers import reverse
 from django.db import models
@@ -424,6 +427,107 @@ def account_links(request):
 def contact(request):
     context = RequestContext(request, {})
     return render_to_response('content/search_results.html', context)
+
+def comment_add(request):
+    """
+    Post a comment. Taken from django.contrib.comments.views.comments and adjusted for ajax submition and anonymous users
+    """
+    # Fill out some initial data fields from an authenticated user, if present
+    data = request.POST.copy()
+    if request.user.is_authenticated():
+        if not data.get('name', ''):
+            data["name"] = request.user.get_full_name() or request.user.username
+        if not data.get('email', ''):
+            data["email"] = request.user.email
+    
+    if not request.user.is_authenticated():
+        if not data.get('name', ''):
+            data["name"] = 'Anonymous'
+        if not data.get('email', ''):
+            data["email"] = 'anonymous@anonymous.com'
+
+    # Look up the object we're trying to comment about
+    ctype = data.get("content_type")
+    object_pk = data.get("object_pk")
+    if ctype is None or object_pk is None:
+        return CommentPostBadRequest("Missing content_type or object_pk field.")
+    try:
+        model = models.get_model(*ctype.split(".", 1))
+        target = model._default_manager.get(pk=object_pk)
+    except TypeError:
+        return CommentPostBadRequest(
+            "Invalid content_type value: %r" % escape(ctype))
+    except AttributeError:
+        return CommentPostBadRequest(
+            "The given content-type %r does not resolve to a valid model." % \
+                escape(ctype))
+    except ObjectDoesNotExist:
+        return CommentPostBadRequest(
+            "No object matching content-type %r and object PK %r exists." % \
+                (escape(ctype), escape(object_pk)))
+
+    # Do we want to preview the comment?
+    preview = "preview" in data
+
+    # Construct the comment form
+    form = comments.get_form()(target, data=data)
+
+    # Check security information
+    if form.security_errors():
+        return CommentPostBadRequest(
+            "The comment form failed security verification: %s" % \
+                escape(str(form.security_errors())))
+
+    # If there are errors or if we requested a preview show the comment
+    if form.errors or preview:
+        form.fields['comment'].error_messages['required'] = 'Please enter your comment'
+        template_list = [
+            "comments/%s_%s_preview.html" % tuple(str(model._meta).split(".")),
+            "comments/%s_preview.html" % model._meta.app_label,
+            "comments/preview.html",
+        ]
+        return render_to_response(
+            template_list, {
+                "comment" : form.data.get("comment", ""),
+                "form" : form,
+                "instance": target,
+                "errors": form.errors
+            },
+            RequestContext(request, {})
+        )
+
+    # Otherwise create the comment
+    comment = form.get_comment_object()
+    comment.ip_address = request.META.get("REMOTE_ADDR", None)
+    if request.user.is_authenticated():
+        comment.user = request.user
+
+    # Signal that the comment is about to be saved
+    responses = signals.comment_will_be_posted.send(
+        sender  = comment.__class__,
+        comment = comment,
+        request = request
+    )
+
+    for (receiver, response) in responses:
+        if response == False:
+            return CommentPostBadRequest(
+                "comment_will_be_posted receiver %r killed the comment" % receiver.__name__)
+
+    # Save the comment and signal that it was saved
+    comment.save()
+    signals.comment_was_posted.send(
+        sender  = comment.__class__,
+        comment = comment,
+        request = request
+    )
+    # Ajx additions
+    context = RequestContext(request, {})
+    context.update({
+        'instance': target
+    })
+    from templatetags.inclusion_tags import CommentsNode
+    return HttpResponse(CommentsNode('instance').render(context))
 
 def logout(request):
     if request.user.is_authenticated():
