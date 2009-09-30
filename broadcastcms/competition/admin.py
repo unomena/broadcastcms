01c496733b872cca3e476e516f09432ea3386c5f
Shaun Sephton <eddied@noidea.(none)>
2009-09-30 12:16:00 +0200

    refactored for model update, cleanup

--- broadcastcms/competition/admin.py
+++ broadcastcms/competition/admin.py
@@ -27,11 +27,15 @@ class WinnerInline(ModelBaseTabularInline):
 
 
 class CompetitionAdmin(ContentBaseAdmin):
-    
     fieldsets = deepcopy(ContentBaseAdmin.fieldsets)
     for fieldset in fieldsets:
         if fieldset[0] == None:
-            fieldset[1]['fields'] += ('content', 'rules', 'question', 'start', 'end')
+            fieldset[1]['fields'] += ('content', 'rules', 'start', 'end')
+    fieldsets += (
+        ('Q&A', {'fields': ('question', 'question_blurb', 'correct_answer',),
+                  'classes': ('collapse',),
+        }),
+    )
     
     inlines = (
         OptionInline, 
