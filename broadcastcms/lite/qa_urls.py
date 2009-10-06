981bae0243a0a6f958acb0888d55f55776b582d0
Shaun Sephton <eddied@noidea.(none)>
2009-10-06 11:09:50 +0200

    cleanup

--- broadcastcms/lite/qa_urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf import settings
-from django.conf.urls.defaults import *
-
-import production_urls
-
-if settings.DEBUG:
-    urlpatterns = production_urls.urlpatterns + patterns('',
-        (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
-    )
