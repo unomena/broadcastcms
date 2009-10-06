f3104affcdf2425b1410825e4956e98ccd0f997b
Shaun Sephton <eddied@noidea.(none)>
2009-10-06 11:12:58 +0200

    comment

--- broadcastcms/lite/middleware.py
+++ broadcastcms/lite/middleware.py
@@ -2,6 +2,12 @@ from django.conf import settings
 
 class URLSwitchMiddleware(object):
     def process_request(self, request):
+        """
+        Alters urlconf module used based on hostname, thus enabling the 
+        delivery of different portals for desktop and mobile clients.
+        TODO: Refactor to use DeviceAtlas or WURFL type device recognition 
+        to determine switch
+        """
         host = request.META['HTTP_HOST'].split(':')[0]
         if host in settings.DESKTOP_HOSTNAMES:
             settings.ROOT_URLCONF = 'broadcastcms.lite.desktop_urls'
