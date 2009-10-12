bd091325259a1bad4ba77f6b09e448657cb745cb
eddied <eddied@noidea.(none)>
2009-10-12 12:52:50 +0200

    cache settings cp

--- broadcastcms/lite/context_processors.py
+++ broadcastcms/lite/context_processors.py
@@ -1,10 +1,13 @@
 from models import Settings
+from broadcastcms.cache.decorators import cache_context_processor
 
+@cache_context_processor(60*10)
 def settings(request):
     settings = Settings.objects.all()
     if len(settings): settings = settings[0]
     else: settings = Settings.objects.create()
     return {'settings': settings}
+
 
 def section(request):
     SECTIONS = ['home', 'shows', 'chart', 'competitions', 'news', 'events', 'galleries']
