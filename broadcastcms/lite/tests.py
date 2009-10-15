cb9bafd08c028d8a935ff3c70576c0eee5b56066
eddied <eddied@noidea.(none)>
2009-10-15 15:50:37 +0200

    a few more tests

--- broadcastcms/lite/tests.py
+++ broadcastcms/lite/tests.py
@@ -1,13 +1,18 @@
 import unittest
 
 from django.conf import settings
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
+from django.template import RequestContext
 from django.test.client import Client
 from django.test import TestCase
 
 from broadcastcms.test.mocks import RequestFactory
 
+from templatetags.desktop_inclusion_tags import *
 
-class ContextProcessorTestCase(unittest.TestCase):
+
+class MiddlewareTestCase(TestCase):
     def setUp(self):
         from middleware import URLSwitchMiddleware
         self.url_switch = URLSwitchMiddleware()
@@ -32,9 +37,33 @@ class ContextProcessorTestCase(unittest.TestCase):
         self.failUnlessEqual(settings.ROOT_URLCONF, 'broadcastcms.lite.desktop_urls')
 
 
-class ViewsTestCase(TestCase):
+class DesktopViewsTestCase(TestCase):
     def testHome(self):
         response = self.client.get('/')
 
         #Check that response is 200 (OK)
         self.failUnlessEqual(response.status_code, 200)
+
+class DesktopInlcusionTagsTestCase(TestCase):
+    def setUp(self):
+        request = RequestFactory().get('/')
+        self.context = RequestContext(request, {})
+        user = User.objects.create_user('test', 'test@test.com', 'test')
+
+    def testAccountLinks(self):
+        # anonymous users should see sign in text etc
+        response_string = account_links('', '').render(self.context)
+        self.failUnless('Hello Stranger' in response_string)
+        self.failUnless('Sign in' in response_string)
+        self.failUnless('Sign up' in response_string)
+        
+        # authneticated users should see profile text etc
+        self.context['request'].user = authenticate(username='test', password='test')
+        response_string = account_links('', '').render(self.context)
+        self.failUnless('Hello test' in response_string)
+        self.failUnless('Profile' in response_string)
+        self.failUnless('Sign out' in response_string)
+
+    #def testMasthead(self):
+    #    masthead('', '').render(self.context)
+    #    self.failUnlessEqual(1, 200)
