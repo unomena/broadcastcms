8d4e96fb3d8e1d3388dcb91a2d2919b9aa726e06
eddied <eddied@noidea.(none)>
2009-10-15 14:56:56 +0200

    corrected keyerror on meta. now defaults to desktop urls

--- broadcastcms/lite/middleware.py
+++ broadcastcms/lite/middleware.py
@@ -5,12 +5,22 @@ class URLSwitchMiddleware(object):
         """
         Alters urlconf module used based on hostname, thus enabling the 
         delivery of different portals for desktop and mobile clients.
-        TODO: Refactor to use DeviceAtlas or WURFL type device recognition 
+        TODO: Refactor to use DeviceAtlas/WURFL device recognition 
         to determine switch
+        Defaults to desktop urls when uncertain
         """
-        host = request.META['HTTP_HOST']
+        # Default to desktop urls
+        settings.ROOT_URLCONF = 'broadcastcms.lite.desktop_urls'
+
+        # Grab the hostname from request.META
+        meta = request.META
+        if meta.has_key('HTTP_HOST'):
+            host = request.META['HTTP_HOST']
+        else:
+            host = settings.DESKTOP_HOSTNAMES[0]
+        
+        # Switch urls based on hostname
         if host in settings.DESKTOP_HOSTNAMES:
             settings.ROOT_URLCONF = 'broadcastcms.lite.desktop_urls'
         elif host in settings.MOBILE_HOSTNAMES:
             settings.ROOT_URLCONF = 'broadcastcms.lite.mobile_urls'
-        return None
