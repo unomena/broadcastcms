b0accccf109a0aaf8d5fd08ff1045dc6636b4e5a
Alex Gaynor <alex.gaynor@gmail.com>
2009-10-27 14:20:07 -0400

    Clean up the inclusion of messages urls, also overide templates correctly.

--- broadcastcms/lite/desktop_urls.py
+++ broadcastcms/lite/desktop_urls.py
@@ -12,6 +12,23 @@ admin.autodiscover()
 handler404 = 'broadcastcms.lite.desktop_views.handler404'
 handler500 = 'broadcastcms.lite.desktop_views.handler500'
 
+messages_urls = patterns('user_messages.views',
+    url(r'^inbox/$', 'inbox',
+        {'template_name': 'desktop/content/account/messages/inbox.html'},
+        name='messages_inbox'),
+    url(r'^create/$', 'message_create',
+        {'template_name': 'desktop/content/account/messages/create.html'},
+        name='message_create'),
+    url(r'^create/(?P<user_id>\d)/$', 'message_create',
+        {'template_name': 'desktop/content/account/messages/create.html'},
+        name='message_create'),
+    url(r'^thread/(?P<thread_id>\d+)/$', 'thread_detail',
+        {'template_name': 'desktop/content/account/messages/thread_detail.html'},
+        name='messages_thread_detail'),
+    url(r'^thread/(?P<thread_id>\d+)/delete/$', 'thread_delete',
+        name='messages_thread_delete'),
+)
+
 urlpatterns = patterns('',
     url(r'^$', home, name='home'),
     
@@ -27,12 +44,8 @@ urlpatterns = patterns('',
     url(r'^account/friends/$', account_friends, name='account_friends'),
     url(r'^account/friends/find/$', account_friends_find, name='account_friends_find'),
     url(r'^account/friends/response/(\d+)/$', 'friends.views.respond_to_friendship_invitation', {'redirect_to_view': 'account_friends'}, name='account_friends_reply'),
-    url(r'^account/messages/inbox/$', 'user_messages.views.inbox', name='messages_inbox'),
-    url(r'^account/messages/create/$', 'user_messages.views.message_create', name='message_create'),
-    url(r'^account/messagse/create/(?P<user_id>\d)/$', 'user_messages.views.message_create', name='message_create'),
-    url(r'^account/messages/thread/(?P<thread_id>\d+)/$', 'user_messages.views.thread_detail', name='messages_thread_detail'),
-    url(r'^account/messages/thread/(?P<thread_id>\d+)/delete/$', 'user_messages.views.thread_delete', name='messages_thread_delete'),
-
+    url(r'^account/messages/', include(messages_urls)),
+    
     
     url(r'chart/(?P<slug>[\w-]+)??$', chart, name='chart'),
     
