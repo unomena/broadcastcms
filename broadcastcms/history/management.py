044f7e06936841f20d7e4a979bb87dee4a713aa4
Alex Gaynor <alex.gaynor@gmail.com>
2009-10-28 16:56:22 -0400

    Added support for turning friendships into HistoryEvents.

--- broadcastcms/history/management.py
+++ broadcastcms/history/management.py
@@ -4,6 +4,9 @@ into HistoryEvents.
 """
 from django.contrib.comments.signals import comment_was_posted
 from django.contrib.contenttypes.models import ContentType
+from django.db.models.signals import post_save
+
+from friends.models import Friendship
 
 from broadcastcms.history.models import HistoryEvent
 
@@ -14,3 +17,16 @@ def record_comment(sender, comment, request, **kwargs):
         object_id=comment.object_pk)
 
 comment_was_posted.connect(record_comment)
+
+def record_friendship(sender, instance, raw, created, **kwargs):
+    if created:
+        HistoryEvent.objects.create(user=instance.to_user,
+            event_type=HistoryEvent.EVENT_FRIEND,
+            content_type=ContentType.get_for_model(instance.from_user),
+            object_id=instance.from_user.pk)
+        HistoryEvent.objects.create(user=instance.from_user,
+            event_type=HistoryEvent.EVENT_FRIEND,
+            content_type=ContentType.get_for_model(instance.to_user),
+            object_id=instance.to_user.pk)
+
+post_save.connect(record_friendship, sender=Friendship)
