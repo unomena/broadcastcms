f08a0fb1f190b02e8751ae04d01c09ec185b5960
Alex Gaynor <alex.gaynor@gmail.com>
2009-11-03 10:41:16 -0500

    Clean up some long lines.

--- broadcastcms/history/management.py
+++ broadcastcms/history/management.py
@@ -14,21 +14,21 @@ from broadcastcms.history.models import HistoryEvent
 
 def record_comment(sender, comment, request, **kwargs):
     HistoryEvent.objects.create(user=comment.user,
-        event_type=HistoryEvent.EVENT_COMMENT, content_type=comment.content_type,
+        event_type=HistoryEvent.EVENT_COMMENT,
+        content_type=comment.content_type,
         object_id=comment.object_pk)
 
 comment_was_posted.connect(record_comment)
 
 def record_friendship(sender, instance, raw, created, **kwargs):
     if created:
+        ct = ContentType.objects.get_for_model(instance.from_user)
         HistoryEvent.objects.create(user=instance.to_user,
             event_type=HistoryEvent.EVENT_FRIEND,
-            content_type=ContentType.objects.get_for_model(instance.from_user),
-            object_id=instance.from_user.pk)
+            content_type=ct, object_id=instance.from_user.pk)
         HistoryEvent.objects.create(user=instance.from_user,
             event_type=HistoryEvent.EVENT_FRIEND,
-            content_type=ContentType.objects.get_for_model(instance.to_user),
-            object_id=instance.to_user.pk)
+            content_type=ct, object_id=instance.to_user.pk)
 
 post_save.connect(record_friendship, sender=Friendship)
 
