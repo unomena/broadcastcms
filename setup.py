48f925c68cc9c6770b09749cc2d1ee0f39114db2
eddied <eddied@noidea.(none)>
2009-11-03 13:05:04 +0200

    eggified

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='broadcastcms',
+    version='dev',
+    #version='1.0.9',
+    description='Broadcast CMS Django Applications',
+    author='Praekelt Consulting',
+    author_email='sysadmin@praekeltconsulting.com',
+    url='http://projects.praekelt.com/broadcastcms',
+    packages = find_packages(),
+    dependency_links = [
+        'http://dist.repoze.org/',
+    ],
+    install_requires = [
+        'django-debug-toolbar==0.8.0',
+        'django-voting==0.1-pre',
+        'MySQL-python==1.2.2',
+        'pexpect==2.4',
+        'python-memcached==1.44',
+        'PIL==1.1.6',
+        'simplejson==2.0.9',
+        'windmill==1.3',
+    ],
+    include_package_data=True,    
+)
