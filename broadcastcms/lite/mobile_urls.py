849f2f8a58291ddde57fa89f04234d73308e8aee
Jonathan Bydendyk <jonathan@jonathan-vb-ubuntu.(none)>
2009-11-04 14:02:24 +0200

    added static urls and generic render_to_response

--- broadcastcms/lite/mobile_urls.py
+++ broadcastcms/lite/mobile_urls.py
@@ -1,11 +1,27 @@
 from django.conf import settings
 from django.conf.urls.defaults import *
+from django.shortcuts import render_to_response
+from django.template import RequestContext
 
-from mobile_views import *
+# Custom direct to template
+def direct_to_template(request, template, name=None):
+    context = RequestContext(request, {})
+    return render_to_response(template, context)
 
+
+# Url patterns
 urlpatterns = patterns('',
-    url(r'^$', home, name='home'),
+    (r'^$', direct_to_template, {'template':'mobile/content/home.html'}),
+    (r'shows/$', direct_to_template, {'template':'mobile/content/shows.html'}),
+    (r'news/$', direct_to_template, {'template':'mobile/content/news.html'}),
+    (r'events/$', direct_to_template, {'template':'mobile/content/events.html'}),
+    (r'competions/$', direct_to_template, {'template':'mobile/content/comps.html'}),
+    (r'galleries/$', direct_to_template, {'template':'mobile/content/galleries.html'}),
+    (r'about/$', direct_to_template, {'template':'mobile/content/about.html'}),
+    (r'contact/$', direct_to_template, {'template':'mobile/content/contact.html'}),
+    (r'sign-in/$', direct_to_template, {'template':'mobile/content/sign-in.html'}),
 )
+
 
 if settings.SERVE_STATIC:
     urlpatterns += patterns('',
