7ec6b158c82d834e0e78e68449c20b70af8ba415
Brian Rosner <brosner@gmail.com>
2009-11-11 11:46:18 -0700

    first pass at management command for making StatusUpdate objects

--- broadcastcms/lite/models.py
+++ broadcastcms/lite/models.py
@@ -2,8 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
 
-import twitter
-
 from broadcastcms.banner.models import Banner
 from broadcastcms.base.models import ContentBase
 from broadcastcms.event.models import Province
