83a43fc6119b01791baca3699b5c9c59c3c1ed41
Shaun Sephton <shaun@praekeltconsulting.com>
2010-01-12 16:56:42 +0200

    reworked chart admin to not use inline song admin. [#1 state:resolved]

--- broadcastcms/chart/admin.py
+++ broadcastcms/chart/admin.py
@@ -4,15 +4,13 @@ from django.forms import models
 from broadcastcms.base.admin import ContentBaseAdmin, ModelBaseTabularInline
 from broadcastcms.chart.models import Chart, ChartEntry
 
-class ChartEntryInline(ModelBaseTabularInline):
-    model = ChartEntry
-    fk_name = 'chart'
-    exclude = ['labels',]
-
-
 class ChartAdmin(ContentBaseAdmin):
-    inlines = (ChartEntryInline,)
-    save_on_top = True
+    pass
 
+class ChartEntryAdmin(admin.ModelAdmin):
+    list_display = ('song', 'chart', 'current_position', 'previous_position', 'is_public',)
+    list_filter = ('chart', 'is_public',)
+    search_fields = ('song__title', 'chart__title')
 
 admin.site.register(Chart, ChartAdmin)
+admin.site.register(ChartEntry, ChartEntryAdmin)
