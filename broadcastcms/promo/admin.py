89645add8a826311e7ef92f7325b697a466aaa56
Shaun Sephton <shaun@praekeltconsulting.com>
2010-01-15 10:37:30 +0200

    dropped inline in favour of standalone

--- broadcastcms/promo/admin.py
+++ broadcastcms/promo/admin.py
@@ -3,13 +3,7 @@ from copy import deepcopy
 from django.contrib import admin
 
 from models import *
-from broadcastcms.base.admin import ModelBaseAdmin, ModelBaseTabularInline
-
-class PromoWidgetSlotInline(ModelBaseTabularInline):
-    model = PromoWidgetSlot
-    fk_name = 'widget'
-    exclude = ['labels',]
-    extra = 2
+from broadcastcms.base.admin import ModelBaseAdmin
 
 class PromoWidgetAdmin(ModelBaseAdmin):
     list_display = ('title',) + ModelBaseAdmin.list_display
@@ -17,9 +11,13 @@ class PromoWidgetAdmin(ModelBaseAdmin):
     for fieldset in fieldsets:
         if fieldset[0] == None:
             fieldset[1]['fields'] += ('title',)
-    inlines = (
-        PromoWidgetSlotInline, 
-    )
-    save_on_top = True
+
+class PromoWidgetSlotAdmin(ModelBaseAdmin):
+    list_display = ('title', 'widget',) + ModelBaseAdmin.list_display
+    fieldsets = deepcopy(ModelBaseAdmin.fieldsets)
+    for fieldset in fieldsets:
+        if fieldset[0] == None:
+            fieldset[1]['fields'] += ('title', 'widget', 'content')
 
 admin.site.register(PromoWidget, PromoWidgetAdmin)
+admin.site.register(PromoWidgetSlot, PromoWidgetSlotAdmin)
