1613bf0eefb3d25cf92fad2265ed83b3ce98c752
Shaun Sephton <shaun@praekeltconsulting.com>
2010-01-15 10:14:42 +0200

    cache can now respect attrs

--- broadcastcms/cache/decorators.py
+++ broadcastcms/cache/decorators.py
@@ -1,7 +1,7 @@
 from django.core.cache import cache
 import md5 
 
-def get_request_key(function, request, respect_path, respect_get, respect_user):
+def get_request_key(function, self, request, respect_path, respect_get, respect_user, respect_self_attrs=[]):
     """
     TODO: Seems a little flacky :), refactor.
     """
@@ -16,14 +16,17 @@ def get_request_key(function, request, respect_path, respect_get, respect_user):
     if respect_user:
         user_key = str(request.user)
         key += user_key
+    for attr in respect_self_attrs:
+        attr_key = str(getattr(self, attr))
+        key += attr_key
     
     key = md5.new(key).hexdigest()
     return key
 
-def cache_view_function(seconds, respect_path=False, respect_get=False, respect_user=False):
+def cache_view_function(seconds, respect_path=False, respect_get=False, respect_user=False, respect_self_attrs=[]):
     def wrap(f):
         def wrap_f(self, context):
-            key = get_request_key(f, context['request'], respect_path, respect_get, respect_user)
+            key = get_request_key(f, self, context['request'], respect_path, respect_get, respect_user, respect_self_attrs)
             cached_result = cache.get(key)
             if cached_result:
                 return cached_result
@@ -37,7 +40,7 @@ def cache_view_function(seconds, respect_path=False, respect_get=False, respect_
 def cache_context_processor(seconds, respect_path=False, respect_get=False, respect_user=False):
     def wrap(f):
         def wrap_f(request):
-            key = get_request_key(f, request, respect_path, respect_get, respect_user)
+            key = get_request_key(f, None, request, respect_path, respect_get, respect_user)
             cached_result = cache.get(key)
             if cached_result:
                 return cached_result
