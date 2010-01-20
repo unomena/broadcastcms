55ed0e8db53418cf2b10996b05ba440823ea2df9
Shaun Sephton <shaun@praekeltconsulting.com>
2010-01-20 12:55:25 +0200

    removed current_status context processor. added utils and widgets apps

--- broadcastcms/lite/production_settings.py
+++ broadcastcms/lite/production_settings.py
@@ -76,7 +76,6 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     'broadcastcms.lite.context_processors.section',
     'broadcastcms.lite.context_processors.site',
     'broadcastcms.lite.context_processors.facebook',
-    'broadcastcms.status.context_processors.current_status',
 )
 
 TEMPLATE_DIRS = (
@@ -128,6 +127,8 @@ INSTALLED_APPS = (
     'broadcastcms.activity',
     'broadcastcms.status',
     'broadcastcms.lite',
+    'broadcastcms.utils',
+    'broadcastcms.widgets',
 )
 
 ABSOLUTE_URL_OVERRIDES = {
