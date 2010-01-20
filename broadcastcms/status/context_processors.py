23517208d3ada86365962c10f889306806e46a62
Shaun Sephton <shaun@praekeltconsulting.com>
2010-01-20 12:47:38 +0200

    blank line remove

--- broadcastcms/status/context_processors.py
+++ broadcastcms/status/context_processors.py
@@ -1,6 +1,5 @@
 from broadcastcms.status.models import StatusUpdate
 
-
 def current_status(request):
     if request.user.is_authenticated():
         status = StatusUpdate.objects.current_status(request.user)
