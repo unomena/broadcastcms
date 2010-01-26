456e1aa48c99ae285db03fee49c532580022f9fd
Shaun Sephton <shaun@praekeltconsulting.com>
2010-01-26 05:51:41 +0200

    added djangofacebook and pyfacebook deps

--- setup.py
+++ setup.py
@@ -14,11 +14,13 @@ setup(
     ],
     install_requires = [
         'django-debug-toolbar==0.8.0',
+        'Django_FacebookConnect==0.2',
         'django-friends==0.2alpha',
         'django-haystack',
         'django-voting==0.1-pre',
         'mysql-python==1.2.3c1',
         'pexpect==2.4',
+        'pyfacebook==0.1',
         'python-dateutil==1.4.1',
         'python-memcached==1.44',
         'python-twitter==0.6',
@@ -27,6 +29,7 @@ setup(
         'user-messages==0.1-dev1',
         'whoosh==0.3.5',
         'windmill==1.3',
+
     ],
     include_package_data=True,
 )
