1e7136e8447d85b8b5137583e7fe638879dc0a2d
Shaun Sephton <shaun@praekelt.com>
2010-02-16 08:46:23 +0200

    refactored and added cp cacher

--- broadcastcms/cache/decorators.py
+++ broadcastcms/cache/decorators.py
@@ -1,12 +1,11 @@
 from django.core.cache import cache
 import md5 
 
-def get_inclusion_view_function_cache_key(node, context, respect_path, respect_get, f):
+def get_request_key(function, self, request, respect_path, respect_get, respect_user, respect_self_attrs=[]):
     """
-    TODO: Refactor to generate keys more succinctly.
+    TODO: Seems a little flacky :), refactor.
     """
-    key = str(node.__class__) + str(f.__name__)
-    request = context['request']
+    key = str(hash(function))
     if respect_path:
         path_key = str(request.path)
         key += path_key
@@ -14,19 +13,39 @@ def get_inclusion_view_function_cache_key(node, context, respect_path, respect_g
         #TODO: Generate a proper get_key instead of a simple string
         get_key = str(request.GET)
         key += get_key
+    if respect_user:
+        user_key = str(request.user)
+        key += user_key
+    for attr in respect_self_attrs:
+        attr_key = str(getattr(self, attr))
+        key += attr_key
     
     key = md5.new(key).hexdigest()
     return key
 
-def cache_inclusion_view_function(seconds, respect_path=False, respect_get=False):
+def cache_view_function(seconds, respect_path=False, respect_get=False, respect_user=False, respect_self_attrs=[]):
     def wrap(f):
         def wrap_f(self, context):
-            key = get_inclusion_view_function_cache_key(self, context, respect_path, respect_get, f)
+            key = get_request_key(f, self, context['request'], respect_path, respect_get, respect_user, respect_self_attrs)
             cached_result = cache.get(key)
             if cached_result:
                 return cached_result
             else:
                 result = f(self, context)
+                cache.set(key, result, seconds)
+                return result
+        return wrap_f
+    return wrap
+
+def cache_context_processor(seconds, respect_path=False, respect_get=False, respect_user=False):
+    def wrap(f):
+        def wrap_f(request):
+            key = get_request_key(f, None, request, respect_path, respect_get, respect_user)
+            cached_result = cache.get(key)
+            if cached_result:
+                return cached_result
+            else:
+                result = f(request)
                 cache.set(key, result, seconds)
                 return result
         return wrap_f
