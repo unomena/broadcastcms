ca518cc23b12f67b231a41126ef438ad3ba165bf
Shaun Sephton <shaun@praekelt.com>
2010-02-22 10:56:41 +0200

    import escape resolving omment security failure

--- broadcastcms/lite/desktop_views.py
+++ broadcastcms/lite/desktop_views.py
@@ -15,6 +15,7 @@ from django.http import Http404, HttpResponse, HttpResponseRedirect
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
 from django.template.loader import render_to_string
+from django.utils.html import escape
 from django.utils.http import urlencode
 from django.views.generic import list_detail
 
