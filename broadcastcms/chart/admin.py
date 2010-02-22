7842f11377f9c8d31c19b80abe8ed38d3d59e7d2
Shaun Sephton <shaun@praekelt.com>
2010-02-22 15:11:46 +0200

    added auto update charts

--- broadcastcms/chart/admin.py
+++ broadcastcms/chart/admin.py
@@ -1,18 +1,34 @@
+from copy import deepcopy
+
 from django.contrib import admin
 from django.forms import models
 
 from broadcastcms.base.admin import ContentBaseAdmin, ModelBaseTabularInline
-from broadcastcms.chart.models import Chart, ChartEntry
+from broadcastcms.chart.models import AutoUpdateChart, AutoUpdateChartEntry, Chart, ChartEntry
 
 class ChartEntryInline(ModelBaseTabularInline):
     model = ChartEntry
     fk_name = 'chart'
     exclude = ['labels', 'created']
 
-
 class ChartAdmin(ContentBaseAdmin):
     inlines = (ChartEntryInline,)
     save_on_top = True
 
+class AutoUpdateChartEntryInline(ModelBaseTabularInline):
+    model = AutoUpdateChartEntry
+    fk_name = 'chart'
+    exclude = ['labels', 'created']
 
+class AutoUpdateChartAdmin(ContentBaseAdmin):
+    inlines = (AutoUpdateChartEntryInline,)
+    
+    fieldsets = deepcopy(ContentBaseAdmin.fieldsets)
+    for fieldset in fieldsets:
+        if fieldset[0] == None:
+            fieldset[1]['fields'] += ('update_at',)
+    
+    save_on_top = True
+
+admin.site.register(AutoUpdateChart, AutoUpdateChartAdmin)
 admin.site.register(Chart, ChartAdmin)
--- broadcastcms/chart/models.py
+++ broadcastcms/chart/models.py
@@ -12,7 +12,6 @@ class Chart(ContentBase):
         verbose_name = 'Chart'
         verbose_name_plural = 'Charts'
 
-
 class ChartEntry(ModelBase):
     chart = models.ForeignKey(
         Chart, 
@@ -42,3 +41,40 @@ class ChartEntry(ModelBase):
         if not self.id and not self.created:
             self.created = datetime.now()
         super(ChartEntry, self).save(*args, **kwargs)
+
+class AutoUpdateChart(Chart):
+    update_at = models.DateTimeField(
+        blank=True,
+        null=True,
+        help_text="Date and time at which to automatically update this chart. Updating iterates over chart entries and sets previous positions to current positions and current positions to next positions, whilst clearing next positions."
+    )
+    
+    class Meta:
+        verbose_name = 'Auto Update Chart'
+        verbose_name_plural = 'Auto Update Charts'
+
+    def update_entries(self):
+        """
+        Updates chart entries. 
+        Previous positions become current positions. 
+        Current positions becomes next positions.
+        Next positions are set to None.
+        update_at field is set to None.
+        """
+        if self.update_at:
+            if (datetime.now() >= self.update_at):
+                for entry in self.chartentries.all():
+                    entry = entry.as_leaf_class()
+                    entry.previous_position = entry.current_position
+                    if entry.next_position != None:
+                        entry.current_position = entry.next_position
+                        entry.next_position = None
+                    entry.save()
+                    self.update_at = None
+                    self.save()
+
+class AutoUpdateChartEntry(ChartEntry):
+    next_position = models.IntegerField(
+        blank=True,
+        null=True,
+    )
