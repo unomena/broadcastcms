f58037a818d1ccba2ac46ed9278a8ff4761335af
Shaun Sephton <shaun@praekelt.com>
2010-03-30 21:09:35 +0200

    switch to text field

--- broadcastcms/post/admin.py
+++ broadcastcms/post/admin.py
@@ -1,11 +1,21 @@
 from copy import deepcopy
 
+from django import forms
 from django.contrib import admin
 
 from broadcastcms.base.admin import ContentBaseAdmin
 from models import Post
 
+from tinymce.widgets import TinyMCE
+
+class PostAdminForm(forms.ModelForm):
+    content = forms.CharField(widget=TinyMCE(attrs={'cols': 80, 'rows': 30}))
+
+    class Meta:
+        model = Post
+
 class PostAdmin(ContentBaseAdmin):
+    form = PostAdminForm
     fieldsets = deepcopy(ContentBaseAdmin.fieldsets)
     for fieldset in fieldsets:
         if fieldset[0] == None:
