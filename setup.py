1d9d03df3408d766672a9eca0934105f4413765b
Shaun Sephton <shaun@praekelt.com>
2010-05-03 14:43:34 +0200

    dev ver rev

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup, find_packages
 
 setup(
     name='broadcastcms',
-    #version='dev',
-    version='goodhopefm.1.0.1',
+    version='dev',
+    #version='goodhopefm.1.0.1',
     description='Broadcast CMS Django Applications',
     author='Praekelt Consulting',
     author_email='sysadmin@praekeltconsulting.com',
