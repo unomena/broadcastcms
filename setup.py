fd54cc19d28084081e6994aed22255abde3ee2ba
shaunsephton <shaun@praekeltconsulting.com>
2010-11-18 16:22:11 +0200

    version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='broadcastcms',
     description='Broadcast CMS Django App',
-    version='sabc3.0.1.5',
+    version='sabc3.1.0.5',
     author=' Praekelt Consulting',
     license='Propriatory',
     packages = find_packages(),
