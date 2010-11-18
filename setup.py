e900749b580d326056c56e453b737a402a3e55e5
shaunsephton <shaun@praekeltconsulting.com>
2010-11-18 16:21:21 +0200

    eggify

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='broadcastcms',
+    description='Broadcast CMS Django App',
+    version='sabc3.0.1.5',
+    author=' Praekelt Consulting',
+    license='Propriatory',
+    packages = find_packages(),
+    zip_safe=False,
+)
